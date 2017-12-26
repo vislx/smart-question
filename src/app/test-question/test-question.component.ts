36148099bb767a7d0e3697b2f8cb3228811d350a
victor <github@vislx.com>
2017-12-26 18:25:57 +0800

    create test question view

--- src/app/test-question/test-question.component.ts
+++ src/app/test-question/test-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,14 +12,20 @@ export class TestQuestionComponent implements OnInit {
   user: string;
   key: string;
   sub = [];
+  
+  public answerForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private fb: FormBuilder) {
     this.sub[0] = this.route.params.subscribe(params => {
       this.user = params['user'];
     });
     this.sub[1] = this.route.queryParams.subscribe(queryParams => {
       this.key = queryParams.key;
     });
+    this.answerForm = fb.group({
+      'user_answer': ['', Validators.required]
+    })
   }
 
   ngOnInit() {
