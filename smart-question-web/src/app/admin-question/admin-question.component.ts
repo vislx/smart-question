7be33f5623b2450f26f7a27528fdf42c55502723
victor <github@vislx.com>
2017-12-29 18:07:14 +0800

    Frontend: admin side page

--- smart-question-web/src/app/admin-question/admin-question.component.ts
+++ smart-question-web/src/app/admin-question/admin-question.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminQuestionComponent implements OnInit {
 
+  public questions = [];
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < 50; i++) {
+      this.questions.push({
+        'id': 'abdewjlajf390u0',
+        'question_name': 'Long-Question 1',
+        'question_body': 'How is the weather today? Please answer in below box. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'preset_answer': [
+          'Today is a nice day! I like the sunshine',
+          'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
+        ]
+      });
+    }
+    
   }
 
 }
